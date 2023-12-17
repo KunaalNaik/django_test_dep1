2b8828d1a6fa5439bea402f7ffa855d5d880f7f1
Kunaal Naik <33804703+KunaalNaik@users.noreply.github.com>
2023-12-17 12:47:55 +0530

    initial

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g)783oj6ee1240pt2uawm)v6f92u$rh0s80w$qn_r=q-cphu-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'helloworldapp.apps.HelloworldappConfig'
 ]
 
 MIDDLEWARE = [
